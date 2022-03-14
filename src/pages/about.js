656b4f7017d473f879cf5c0e69ad026181140614
Link1986 <ali.ghoudi@gmail.com>
2022-03-14 21:38:43 +0100

    add some files to projects folder

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero cupiditate saepe eos autem odio illum dicta esse, fugit qui ea aliquam amet tempore maxime sint impedit expedita dolores iste? Dolore?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero cupiditate saepe eos autem odio illum dicta esse, fugit qui ea aliquam amet tempore maxime sint impedit expedita dolores iste? Dolore?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero cupiditate saepe eos autem odio illum dicta esse, fugit qui ea aliquam amet tempore maxime sint impedit expedita dolores iste? Dolore?</p>
+    </div>
+  )
+}
